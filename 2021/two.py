e33d26026cbb8576e30e447db7227cef626b8de4
Amos <herzamosrgmail.com>
2021-12-02 10:30:40 +0100

    Day 2 part 1 restored

--- 2021/two.py
+++ 2021/two.py
@@ -1,3 +1,18 @@
+def partOne():
+    h = d = 0
+    with open("two.txt", "r") as f:
+        for line in f.readlines():
+            dir = line.rstrip("\n").split()[0]
+            x = int(line.split()[1])
+            match dir:
+                case "forward":
+                    h += x
+                case "up":
+                    d -= x
+                case "down":
+                    d += x
+    print(h*d)
+
 def partTwo():
     h = d = a = 0
     with open("two.txt", "r") as f:
@@ -15,6 +30,7 @@ def partTwo():
     print(h*d)
     
 if __name__ == "__main__":
+    partOne()
     partTwo()
 
                     
